51109941b838f803be73ae13c2397a1e88dd933f
Muskan Arora <muskanarora@Muskans-MacBook-Pro.local>
2025-12-28 22:01:40 -0500

    reverting changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,7 +18,7 @@ export default function About() {
             <main className='flex w-full flex-col items-center justify-center dark:text-light dark:bg-dark'>
                 <Layout className='pt-16 dark:bg-dark'>
                     <AnimatedText text="Code. Create. Innovate!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
-                    <div className='grid w-full grid-cols-8 gap-16 sm:gap-8 md:flex md:flex-col'>
+                    <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
                         <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'>
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                             <div className="w-full h-[500px]  rounded-2xl flex items-center justify-center">
@@ -44,7 +44,7 @@ export default function About() {
                         <Link href="/experience" className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'>
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                             <div className="w-full h-[500px]  rounded-2xl flex items-center justify-center">
-                                <div className='col-span-3 relative h-max flex flex-col items-start justify-start xl:col-span-4 md:order-1 md:col-span-8'>
+                                <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                                     <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Experience</h2>
                                     <div> <Experience /></div>
 
